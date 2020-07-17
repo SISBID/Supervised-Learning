e51646de9d0878307a111376d2d8ae1e9216cbb3
shojaie <ali.shojaie@gmail.com>
2020-07-16 19:28:52 -0700

    Added Heart data example
    Updated the code with another example examining different classification methods and ROC curves.

--- ClassificationLab.R
+++ ClassificationLab.R
@@ -95,34 +95,7 @@ lasso.pred[lasso.probs>.5]="Up"
 table(lasso.pred,Direction.2005)
 mean(lasso.pred==Direction.2005)
 
-# Linear Discriminant Analysis
-
-library(MASS)
-lda.fit=lda(Direction~Lag1+Lag2,data=Smarket,subset=train)
-lda.fit
-plot(lda.fit)
-lda.pred=predict(lda.fit, Smarket.2005)
-names(lda.pred)
-lda.class=lda.pred$class
-table(lda.class,Direction.2005)
-mean(lda.class==Direction.2005)
-sum(lda.pred$posterior[,1]>=.5)
-sum(lda.pred$posterior[,1]<.5)
-lda.pred$posterior[1:20,1]
-lda.class[1:20]
-sum(lda.pred$posterior[,1]>.9)
-hist(lda.pred$posterior)
-
-# Quadratic Discriminant Analysis
-
-qda.fit=qda(Direction~Lag1+Lag2,data=Smarket,subset=train)
-qda.fit
-qda.class=predict(qda.fit,Smarket.2005)$class
-table(qda.class,Direction.2005)
-mean(qda.class==Direction.2005)
-
 # K-Nearest Neighbors
-
 library(class)
 train.X=cbind(Lag1,Lag2)[train,]
 test.X=cbind(Lag1,Lag2)[!train,]
@@ -136,4 +109,118 @@ table(knn.pred,Direction.2005)
 mean(knn.pred==Direction.2005)
 ############################################
 
+############################################
+## Another example: Classification with the Heart data
+############################################
 
+## Need to load the Heart data, available on github
+load("Heart.rda")
+attach(Heart)
+
+names(Heart)
+dim(Heart)
+summary(Heart)
+plot(trestbps)
+
+# separate the data into training and test
+set.seed(1)
+train=sample(1:nrow(Heart), 197)
+trndat=Heart[train,]
+tstdat=Heart[-train,]
+dim(trndat)
+dim(tstdat)
+
+# Logistic Regression
+HD.tst=HD[-train]
+glm.fit=glm(HD~.,data=Heart,family=binomial,subset=train)
+glm.probs=predict(glm.fit,tstdat,type="response")
+glm.pred=numeric(100)
+glm.pred[glm.probs>.5]=1
+table(glm.pred,HD.tst)
+mean(glm.pred==HD.tst)
+
+# penalized logistic regression
+x=as.matrix(Heart[,1:13])
+y=as.factor(Heart[,14])
+
+#ridge
+glm.ridge=glmnet(x[train,],y[train],family="binomial",alpha=0)
+plot(glm.ridge,col=c(1:8), xvar="lambda")
+legend('topright',colnames(x),lty=1,col=c(1:8),bty='n')
+cv.ridge=cv.glmnet(x[train,],y[train],family="binomial",alpha=0)
+plot(cv.ridge)
+lambda.min=cv.ridge$lambda.min
+ridge.probs=predict(glm.ridge,s=lambda.min,type="response",newx=x[-train,])
+ridge.pred=rep(0,100)
+ridge.pred[ridge.probs>.5]=1
+table(ridge.pred,HD.tst)
+mean(ridge.pred==HD.tst)
+
+#lasso
+glm.lasso=glmnet(x[train,],y[train],family="binomial",alpha=1)
+plot(glm.lasso,col=c(1:8), xvar="lambda")
+legend('bottomright',colnames(x),lty=1,col=c(1:8),bty='n')
+cv.lasso=cv.glmnet(x[train,],y[train],family="binomial",alpha=1)
+plot(cv.lasso)
+lambda.min=cv.lasso$lambda.min
+lasso.probs=predict(glm.lasso,s=lambda.min,type="response",newx=x[-train,])
+lasso.pred=rep(0,100)
+lasso.pred[ridge.probs>.5]=1
+table(lasso.pred,HD.tst)
+mean(lasso.pred==HD.tst)
+
+
+# K-Nearest Neighbors
+
+library(class)
+train.X=cbind(ca,thal)[train,]
+test.X=cbind(ca,thal)[-train,]
+HD.trn=HD[train]
+set.seed(1)
+knn.pred=knn(train.X,test.X,as.factor(HD.trn),k=1)
+table(knn.pred,HD.tst)
+mean(knn.pred==HD.tst)
+knn.pred=knn(train.X,test.X,HD.trn,k=10)
+table(knn.pred,HD.tst)
+mean(knn.pred==HD.tst)
+
+# SVM
+
+dat=Heart
+dat$HD=as.factor(HD)
+svmfit=svm(HD~.,data=dat[train,],kernel="radial",gamma=1,cost=1)
+svm.pred=predict(svmfit,newdata=dat[-train,-14])
+table(true=dat[-train,14], pred=svm.pred)
+mean(dat[-train,14]==svm.pred)
+
+set.seed(1)
+tune.out=tune.svm(x=dat[train,-14],y=dat[train,14],kernel="radial",
+                  cost=c(0.1,1,10,100,1000),gamma=c(0.5,1,2,3,4))
+summary(tune.out)
+svmfit=svm(HD~.,data=dat[train,],kernel="radial",gamma=tune.out$best.parameters$gamma,cost=tune.out$best.parameters$cost)
+svm.pred=predict(svmfit,newdata=dat[-train,-14])
+table(true=dat[-train,14], pred=svm.pred)
+mean(dat[-train,14]==svm.pred)
+
+
+# ROC Curves
+
+rocplot=function(pred, truth, ...){
+  predob = prediction(pred, truth)
+  perf = performance(predob, "tpr", "fpr")
+  plot(perf,...)}
+
+svmfit.opt=svm(y~., data=dat[train,], kernel="radial",gamma=2, cost=1,decision.values=T)
+fitted=attributes(predict(svmfit.opt,dat[train,],decision.values=TRUE))$decision.values
+par(mfrow=c(1,2))
+rocplot(fitted,dat[train,"y"],main="Training Data")
+svmfit.flex=svm(y~., data=dat[train,], kernel="radial",gamma=50, cost=1, decision.values=T)
+fitted=attributes(predict(svmfit.flex,dat[train,],decision.values=T))$decision.values
+rocplot(fitted,dat[train,"y"],add=T,col="red")
+legend('bottomright',c('optimal model','flexible model'),lty=1,col=c(1,2),bty='n')
+fitted=attributes(predict(svmfit.opt,dat[-train,],decision.values=T))$decision.values
+rocplot(fitted,dat[-train,"y"],main="Test Data")
+fitted=attributes(predict(svmfit.flex,dat[-train,],decision.values=T))$decision.values
+rocplot(fitted,dat[-train,"y"],add=T,col="red")
+
+############################################
